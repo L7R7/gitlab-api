de9b2d32f035b065cd1f6393e0a7a398ad30bf03
Leonhard Riedisser <leo008180@googlemail.com>
2023-01-24 17:12:20 +0100

    Run the api tests for single project and single job on several examples

--- test/api/ProjectSpec.hs
+++ test/api/ProjectSpec.hs
@@ -5,6 +5,7 @@ module ProjectSpec (spec) where
 import API
 import Data.Either
 import Data.Either.Extra
+import Data.Foldable (for_)
 import Job qualified
 import Lib
 import Network.HTTP.Types
@@ -19,19 +20,20 @@ spec = beforeAllWith (pure . fmap project) $ describe "project" $ do
     res <- runClientM allProjects clientEnv
     res `shouldSatisfy` isRight
     shouldBeValid $ eitherToMaybe res
-  itWithOuter "single project" $ \(clientEnv, ProjectAPI _ singleProjectApi) -> do
-    let pId = Id @Project 720 -- todo: make this more robust by using different projects
-    let (SingleProjectAPI singleProject _) = singleProjectApi pId
-    res <- runClientM singleProject clientEnv
-    shouldBeValid res
-    (projectId <$> res) `shouldSatisfy` elem pId
-  itWithOuter "single job" $ \(clientEnv, ProjectAPI _ singleProjectApi) -> do
-    let pId = Id @Project 720 -- todo: make this more robust by using different projects
-    let jId = Id @Job.Job 4028634
-    let (SingleProjectAPI _ (JobAPI singleJob)) = singleProjectApi pId
-    res <- runClientM (singleJob jId) clientEnv
-    shouldBeValid res
-    (Job.jobId <$> res) `shouldSatisfy` all (jId ==)
+  describe "single project" $
+    for_ [Id @Project 720, Id 795, Id 3015, Id 2679] $ \pId -> itWithOuter (show pId) $ \(clientEnv, ProjectAPI _ singleProjectApi) -> do
+      let (SingleProjectAPI singleProject _) = singleProjectApi pId
+      res <- runClientM singleProject clientEnv
+      shouldBeValid res
+      (projectId <$> res) `shouldSatisfy` elem pId
+  describe "single job" $
+    -- todo: Id 4117230. a skipped job has no started_at
+    for_ [Id @Job.Job 4028634, Id 4117227, Id 4114940] $ \jId -> itWithOuter (show jId) $ \(clientEnv, ProjectAPI _ singleProjectApi) -> do
+      let pId = Id @Project 720
+      let (SingleProjectAPI _ (JobAPI singleJob)) = singleProjectApi pId
+      res <- runClientM (singleJob jId) clientEnv
+      shouldBeValid res
+      (Job.jobId <$> res) `shouldSatisfy` all (jId ==)
 
 -- todo: move me to a better place and consider sharing the instance with the one from Utils.hs in unit-test
 instance GenValid (Id a)
