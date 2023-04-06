bc3ea4bf39b83e6e2aec0853003543276bd274e1
Leonhard Riedisser <leo008180@googlemail.com>
2023-04-06 10:48:26 +0200

    Use beforeAll instead of setupAroundAll

--- test/api/Main.hs
+++ test/api/Main.hs
@@ -10,28 +10,28 @@ import System.Environment (lookupEnv)
 import Test.Syd
 
 main :: IO ()
-main = sydTest $ modifyMaxSuccess (const 5) $ setupAroundAll ((,) <$> clientEnvSetupFunc <*> gitlabAPISetupFunc) spec
+main = sydTest $ beforeAll ((,) <$> clientEnvSetup <*> gitlabAPIClient) spec
 
-privateTokenSetupFunc :: SetupFunc String
-privateTokenSetupFunc = SetupFunc $ \func -> do
+privateTokenSetup :: IO String
+privateTokenSetup = do
   maybeToken <- lookupEnv "GITLAB_API_TOKEN"
   case maybeToken of
     Nothing -> expectationFailure "Expecting the API token for the requests in the environment variable \"GITLAB_API_TOKEN\""
-    Just token -> func token
+    Just token -> pure token
 
-gitlabAPISetupFunc :: SetupFunc (GitlabAPI (AsClientT ClientM))
-gitlabAPISetupFunc = client (Proxy @API) . Just <$> privateTokenSetupFunc
+gitlabAPIClient :: IO (GitlabAPI (AsClientT ClientM))
+gitlabAPIClient = client (Proxy @API) . Just <$> privateTokenSetup
 
-baseUrlSetupFunc :: SetupFunc String
-baseUrlSetupFunc = SetupFunc $ \func -> do
+baseUrlSetup :: IO String
+baseUrlSetup = do
   maybeBaseUrl <- lookupEnv "CI_SERVER_HOST"
   case maybeBaseUrl of
     Nothing -> expectationFailure "Expecting the base URL for the requests in the environment variable \"CI_SERVER_HOST\""
-    Just baseUrl' -> func baseUrl'
+    Just baseUrl' -> pure baseUrl'
 
-clientEnvSetupFunc :: SetupFunc ClientEnv
-clientEnvSetupFunc =
-  baseUrlSetupFunc >>= \baseUrlString -> SetupFunc $ \func -> do
+clientEnvSetup :: IO ClientEnv
+clientEnvSetup =
+  baseUrlSetup >>= \baseUrlString -> do
     manager' <- newManager tlsManagerSettings
     baseUrl' <- parseBaseUrl baseUrlString
-    func $ mkClientEnv manager' baseUrl'
+    pure $ mkClientEnv manager' baseUrl'
